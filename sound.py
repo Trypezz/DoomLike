351dbb07dcca62a3c4226c588bcc8c57c77e2ace
Trypezz <89207551+Trypezz@users.noreply.github.com>
2023-06-29 17:54:52 +0200

    Update sound.py

--- sound.py
+++ sound.py
@@ -5,7 +5,7 @@ class Sound:
     def __init__(self, game):
         self.game = game
         pg.mixer.init()
-        self.path = 'C:/Users/madep/Documents/[1] Visual Studio Code/Python/RayCasting_Game/resources/sound/'
+        self.path = 'resources/sound/'
         self.shotgun = pg.mixer.Sound(self.path + 'shotgun.wav')
         self.npc_pain = pg.mixer.Sound(self.path + 'npc_pain.wav')
         self.npc_death = pg.mixer.Sound(self.path + 'npc_death.wav')
